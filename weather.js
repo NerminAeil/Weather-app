8f2a6656ef25b02119fa3713f800e374714125e5
Nermin Aeil <nermeenakil42@gmail.com>
2026-09-11 17:33:54 +0300

    Remove unnecessary blank line in weather.js

--- weather.js
+++ weather.js
@@ -1,7 +1,6 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-
 const locationBtn = document.querySelector("#MyLocation");
 const clearBtn = document.querySelector(".clearBtn");
 const sun_card=document.querySelector(".sun_card");
